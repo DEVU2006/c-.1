a28c6ee6178016bcc8f365c6aa56f61616007d78
DEVU2006 <devendragoudagpatil@gmail.com>
2025-05-13 15:32:36 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,17 @@
+// function defination and function call
+#include <stdio.h>
+int c;
+    int add (int a,int b){
+    //function defination
+    c= a + b ;
+    //function defination
+    return c;
+    }
+int main()    
+{
+    int result = add (7, 8);
+    //function call
+    printf("Result is:%d",c);
+
+    return 0;
+}
