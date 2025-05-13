5af30f59d36936fba06e494c388efdfaab08bb36
DEVU2006 <devendragoudagpatil@gmail.com>
2025-05-13 15:44:38 +0530

    Create college.c

---
+++ college.c
@@ -0,0 +1,15 @@
+// function defination and function call
+#include <stdio.h>
+    int shridevi()
+{        
+    //function defination
+    printf("Devendragouda\n");
+    printf("Date of Brith\n");
+    printf("age\n");
+    printf("ph number");
+    }
+int main()    
+{
+    shridevi();
+    return 0;
+}
