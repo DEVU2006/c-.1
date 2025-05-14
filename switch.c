230e720f1cc4c1ff48d27472ede335cec628f994
DEVU2006 <devendragoudagpatil@gmail.com>
2025-05-14 09:14:51 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int a,
+
+char op;
+
+printf("enter the value of a, op and b"
+
+);
+
+scanf("%d %c %d", &a, &op, &b);
+
+switch(op) {
+
+case '+': printf("%d", a + b); break
+
+case '-': printf("%d", a - b); break
+
+case '*': printf("%d", a * b); break
+
+case '/': printf("%d", a / b); break
+
+default: printf("Invalid operator");
+
+}
+
+}
