f54cbdf6c046ace7ba09db380a3039dd7e17df7f
DEVU2006 <devendragoudagpatil@gmail.com>
2025-05-14 09:11:08 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+- int main(){
+
+char ch;
+
+printf("enter the value of char");
+
+scanf(" %c", &ch);
+
+if (ch=='a'||ch=='e'||ch=='i'||ch =='o'||ch=='u'|| ch=='A'||ch=='E'||ch=='I'||ch=='0'||c =='U')
+
+printf("Vowel");
+
+else
+
+printf("Consonant");
+
+}
