22e5a8b6dddfb9697cfddc8c9667c1579215d74c
DEVU2006 <devendragoudagpatil@gmail.com>
2025-05-16 12:31:17 +0530

    Create revearr.c

---
+++ revearr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int arr[100] ={1,2,3,4,5},n=5,i;
+    int *start, *end, temp;
+    start = arr;
+    end = arr + n-1;
+    while (start>end){
+        temp=*start;
+        *start=*end;
+        *end=temp;
+        start++;
+        end--;
+        
+    }
+    for (i = n -1;i>=0;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+}
